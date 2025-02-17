14afb1aa25af406f0463ece1ee5551c978acd514
mbCeli <munbaporicelina@outlook.com>
2025-02-17 17:51:33 +0000

    Profile layout organised

--- roll-on-line_app/src/pages/HomePage.jsx
+++ roll-on-line_app/src/pages/HomePage.jsx
@@ -32,6 +32,7 @@ export default function HomePage() {
         marginTop: "9.2vh",
       }}
     >
+      {/* Profile Dashboard */}
       <Box
         aria-label="left-side"
         sx={{
@@ -43,35 +44,163 @@ export default function HomePage() {
           height: "88vh",
         }}
       >
-        <Grid2 container spacing={4} columns={12}>
-          <Grid2 size={6} m={1}>
+        {/* Personal Info */}
+        <Grid2
+          container
+          spacing={4}
+          columns={12}
+          sx={{
+            marginTop: "8vh",
+
+            justifyContent: "center",
+            textAlign: "center",
+          }}
+        >
+          <Grid2 size={12} m={1} sx={{ textAlign: "center" }}>
+            <img src={profile.image} alt="user picture" />
+          </Grid2>
+          <Grid2 size={12} m={1} component="h3" sx={{ textAlign: "center" }}>
             {profile.name}
           </Grid2>
-          <Grid2 size={6} m={1}>
-            sad
+          <Grid2 size={3} m={1} sx={{ textAlign: "center" }}>
+            {profile.age}
+          </Grid2>
+          <Grid2 size={3} m={1} sx={{ textAlign: "center" }}>
+            {profile.location}
+          </Grid2>
+          <Grid2 size={3} m={1} sx={{ textAlign: "center" }}>
+            {profile.skating_since}
+          </Grid2>
+
+          {/* Equipment */}
+          <Grid2
+            container
+            size={12}
+            spacing={1}
+            m={1}
+            sx={{ textAlign: "center", justifyContent: "center" }}
+          >
+            <Grid2 size={12} m={2} component="h3">
+              My Equipment
+            </Grid2>
+            <Grid2 size={3} sx={{ textAlign: "center", fontSize: 12 }}>
+              <span style={{ fontWeight: "bold" }}>Skates </span>
+              <br />
+              {profile?.skates?.name}
+              <br />
+              Type - {profile?.skates?.type}
+              <br />
+              <img src={profile?.skates?.image} alt="skates pictures" />
+            </Grid2>
+            <Grid2 size={3} sx={{ textAlign: "center", fontSize: 12 }}>
+              <span style={{ fontWeight: "bold" }}>Helmet </span>
+              <br />
+              {profile?.helmet?.name}
+              <br />
+              Model - {profile?.helmet?.model}
+              <br />
+              <img src={profile?.helmet?.image} alt="helmet pictures" />
+            </Grid2>
+            <Grid2 size={3} sx={{ textAlign: "center", fontSize: 12 }}>
+              <span style={{ fontWeight: "bold" }}>Knee Pads</span>
+              <br />
+              {profile?.knee_pads?.name}
+              <br />
+              Model - {profile?.knee_pads?.model}
+              <br />
+              <img src={profile?.knee_pads?.image} alt="knee_pads pictures" />
+            </Grid2>
+            <Grid2 size={3} sx={{ textAlign: "center", fontSize: 12 }}>
+              <span style={{ fontWeight: "bold" }}>Wrist Guards</span>
+              <br />
+              {profile?.wrist_protectors?.name}
+              <br />
+              Model - {profile?.wrist_protectors?.model}
+              <br />
+              <img
+                src={profile?.wrist_protectors?.image}
+                alt="wrist_protectors pictures"
+              />
+            </Grid2>
+          </Grid2>
+        </Grid2>
+
+        {/* Stats */}
+        <Grid2
+          container
+          size={12}
+          spacing={1}
+          m={1}
+          sx={{ textAlign: "center", justifyContent: "center" }}
+        >
+          <Grid2 size={12} m={2} component="h3">
+            So Far
+          </Grid2>
+          <Grid2 size={3} sx={{ textAlign: "center", fontSize: 12 }}>
+            <span style={{ fontWeight: "bold" }}>Weekly</span>
+            <br />
+            Distance - {profile?.skating_stats?.weekly_distance} m
+            <br />
+            Sessions - {profile?.skating_stats?.weekly_sessions}
+            <br />
+            Time - {profile?.skating_stats?.weekly_time} min
+            <br />
+            Calories - {profile?.skating_stats?.weekly_calories}
+          </Grid2>
+          <Grid2 size={3} sx={{ textAlign: "center", fontSize: 12 }}>
+            <span style={{ fontWeight: "bold" }}>Monthly</span>
+            <br />
+            Distance - {profile?.skating_stats?.monthly_distance} m
+            <br />
+            Sessions - {profile?.skating_stats?.monthly_sessions}
+            <br />
+            Time - {profile?.skating_stats?.monthly_time} min
+            <br />
+            Calories - {profile?.skating_stats?.monthly_calories}
+          </Grid2>
+          <Grid2 size={3} sx={{ textAlign: "center", fontSize: 12 }}>
+            <span style={{ fontWeight: "bold" }}>Yearly Goals</span>
+            <br />
+            Distance - {profile?.skating_goals?.yearly_distance_goal} m
+            <br />
+            Sessions - {profile?.skating_goals?.yearly_sessions_goal}
+            <br />
+            Time - {profile?.skating_goals?.yearly_time_goal} min
+            <br />
+            Calories - {profile?.skating_goals?.yearly_calories_goal}
           </Grid2>
         </Grid2>
       </Box>
 
-      <Box
+      <Stack
         aria-label="right-side"
+        spacing={3}
         sx={{
           marginRight: "1vw",
           marginTop: "1vh",
-          backgroundColor: "primary.light",
           width: "50vw",
           height: "88vh",
         }}
       >
-        <Grid2 container spacing={4} columns={12}>
-          <Grid2 size={6} m={1}>
-            sad
-          </Grid2>
-          <Grid2 size={6} m={1}>
-            sad
-          </Grid2>
-        </Grid2>
-      </Box>
+        <Stack
+          sx={{
+            width: "50vw",
+            height: "44vh",
+            backgroundColor: "primary.light",
+          }}
+        >
+          <Box></Box>
+          <Box></Box>
+          <Box></Box>
+        </Stack>
+        <Box
+          sx={{
+            width: "50vw",
+            height: "44vh",
+            backgroundColor: "primary.light",
+          }}
+        ></Box>
+      </Stack>
     </Stack>
   );
 }
